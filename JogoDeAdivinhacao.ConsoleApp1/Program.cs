cbe9d5e87990868f83041545998c9a4b16808b41
Filipe Peruchi <jfperuchic@gmail.com>
2026-03-23 20:05:34 -0300

    Adicionada multiplas tentativas

--- JogoDeAdivinhacao.ConsoleApp1/Program.cs
+++ JogoDeAdivinhacao.ConsoleApp1/Program.cs
@@ -9,17 +9,47 @@ v1
     Processamento
     O sistema compara o número digitado com um número inteiro aleatório
     Output (Saída de Dados)
-    O sistema informará o usuário se o mesmo acertou ou não, podendo incluir dicas sobre a proximidade do "chute"
+    O sistema informará o usuário se o mesmo acertou ou não, podendo incluir 
+    dicas sobre a proximidade do "chute"
 */
-
+bool jogoDeveContinuar = true;
+int numAleatorio = RandomNumberGenerator.GetInt32(1, 21);
 Console.WriteLine("-------------------------");
 Console.WriteLine("---Jogo de Adivinhação---");
 Console.WriteLine("-------------------------");
 Console.WriteLine();
+while (jogoDeveContinuar == true)
+{
+    Console.Write("Informe um número ->");
+    int numDig = Convert.ToInt32(Console.ReadLine());
+    System.Console.WriteLine();
 
-Console.Write("Informe m número ->");
-string strNumDig = Console.ReadLine();
+    if (numDig == numAleatorio)
+    {
+        System.Console.WriteLine("Parabéns, você acertou! O número era " + numAleatorio);
+        jogoDeveContinuar = false;
+    }
+    else if (numDig > numAleatorio)
+    {
+        System.Console.WriteLine("O número digitado foi maior que o número secreto.");
+    }
+    else if (numDig < numAleatorio)
+    {
+        System.Console.WriteLine("O número digitado foi menor que o número secreto.");
+    }
 
-int numAleatorio = RandomNumberGenerator.GetInt32(1, 21);
+    System.Console.WriteLine();
+    System.Console.Write("Deseja continuar? (s/n): ");
+    string opcCont = Console.ReadLine().ToUpper();
 
-Console.WriteLine("O número digitado é " + numAleatorio);
+    if (opcCont == "S")
+    {
+        jogoDeveContinuar = true;
+    }
+    else if (opcCont == "N")
+    {
+        jogoDeveContinuar = false;
+    }
+
+
+}
